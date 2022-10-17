b17f4446b57999a09fd7feb220ef6a0a67882d5b
onkardeolankar1996@gmail.com <codewithkeeda@gmail.com>
2022-10-17 18:17:45 +0530

    url

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,5 +1,5 @@
 const mongodb = require('mongodb')
 const dbName = 'food-app'
-const dbUrl = `mongodb+srv://Onkar:F5Z6B0KarUxuqzhJ@cluster0.roxwh.mongodb.net/${dbName}?retryWrites=true&w=majority`
+const dbUrl = `process.env.dbUrl`
 
 module.exports = {mongodb,dbName,dbUrl}
